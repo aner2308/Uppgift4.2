7239bc02ac99d6fbeffb4aed89be8ae2f34d01bb
Anton <erikssonanton94@gmail.com>
2024-05-08 09:49:24 +0200

    Lagt till funktion för att kunna registrera användare. Efter registrering ombes man skriva in sina nya uppgifter för att logga in

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,6 @@
 document.getElementById("loginForm").addEventListener("submit", async (event) => {
     event.preventDefault();
-    
+
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
@@ -15,7 +15,7 @@ document.getElementById("loginForm").addEventListener("submit", async (event) =>
 
         let data = await response.json();
 
-        if (response.status === 200) {
+        if (response.ok) {
             // Hantera lyckad inloggning
             const token = data.response.token;
             console.log(token);
@@ -31,3 +31,57 @@ document.getElementById("loginForm").addEventListener("submit", async (event) =>
         alert("Ett fel uppstod. Försök igen senare.");
     }
 });
+
+const registerFormEl = document.getElementById("registerForm");
+
+registerFormEl.addEventListener("submit", registerNew);
+
+async function registerNew(event) {
+    event.preventDefault();
+
+    const usernameEl = document.getElementById("username2").value;
+    const passwordEl = document.getElementById("password2").value;
+
+    if (usernameEl.trim().length < 6) {
+        alert("Användarnamnet måste vara minst 6 tecken långt.")
+        return;
+    }
+
+    if (passwordEl.trim().length < 6) {
+        alert("Lösenordet måste vara minst 6 tecken långt.")
+        return;
+    }
+
+    await newUser(usernameEl, passwordEl);
+
+    document.getElementById("username2").value = "";
+    document.getElementById("password2").value = "";
+}
+
+async function newUser(usernameEl, passwordEl) {
+
+    const url = "https://nodejs-uppgift4-1.onrender.com/api/register";
+
+    let user = {
+        username: usernameEl,
+        password: passwordEl
+    }
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    });
+
+    if (response.ok) {
+        // Visa meddelande om lyckad registrering
+        document.getElementById("registrationMessage").style.display = "block";
+    } else {
+        // Hantera misslyckad registrering
+        alert("Något gick fel!");
+    }
+
+
+}
